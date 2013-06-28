79a6001adf201a6849919835f8787b8e50f69968
Chase Willden <chase.willden@gmail.com>
2013-06-28 09:24:08 -0600

    Update and rename words.php to words.class.php

---
+++ words.class.php
@@ -0,0 +1,43 @@
+<?php
+    
+    /*
+     *    File:			Words.class.php
+     *	Description:	A php script that does anything with words
+     *	Author:			Chase Willden 06/28/2013
+     */
+     
+    class Word{
+        // Declare variables
+        private $word;
+
+        public function __construct($wordArg){
+            $this->word = $wordArg;
+        }
+
+        public function set_word($newWord){
+            $this->word = $newWord;
+        }
+
+        public function get_word(){
+            return $this->word;
+        }
+
+        // returns if valid
+        function is_valid(){
+            $file = file_get_contents('txt/wordlist.txt');
+            $wordAry = explode("\n", $file);
+            $count = 0;
+            for ($i = 0; $i < count($wordAry); $i++){
+                if ($wordAry[$i] == $word){
+                    $count++;
+                    return TRUE;
+                    break;
+                }
+            }
+            if ($count == 0){
+                return FALSE;
+            }
+        }
+
+    }
+?>
